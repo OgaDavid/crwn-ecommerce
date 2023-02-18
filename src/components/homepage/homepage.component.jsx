6170c0ce55ceebc55a7d185fa5b3084293ec68b3
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-02-17 21:54:55 -0800

    created vite app, homepage structure.

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Directory from "../directory/directory.component";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title }) => {
+  return (
+    <div className="menu-item">
+      <div className="content">
+        <div className="title">{ title }</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
