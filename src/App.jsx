6cb312931eb5fb1ee6b76166010412a7012e923b
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-02-19 03:59:35 -0800

    feat: created fixed header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopPage/shopPage.component";
+import Header from "./components/header/header.component";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <Header />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
